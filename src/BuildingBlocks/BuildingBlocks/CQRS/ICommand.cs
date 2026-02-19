92d518f89da94fc0bf5817410ebfdff1591de303
Hassan Marji <h.marji@reply.it>
2026-02-19 17:51:10 +0100

    - completata implementazione CQRS con implementazione interfacce - eliminata dipendenza nel catalog api per mediatR, facendo riferimento al progetto buildingblocks

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    /// <summary>
+    /// Rappresenta un comando che non restituisce un valore.
+    /// </summary>
+    public interface ICommand : IRequest<Unit>
+    {
+
+    }
+
+    /// <summary>
+    /// Rappresenta un comando che restituisce una risposta.
+    /// </summary>
+    /// <typeparam name="TResponse">Il tipo della risposta restituita dal comando.</typeparam>
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
