771ba61497e04a2d1fb9a8b99a46812917dd0705
Hassan Marji <h.marji@reply.it>
2026-02-23 17:56:29 +0100

    - creato endpoint per selezionare tutti i prodotti

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace CatalogAPI.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductResult>;
+    public record GetProductResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductResult>
+    {
+        public async Task<GetProductResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Handling GetProductsRequest called with query{query}");
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductResult(products);
+        }
+    }
+}
