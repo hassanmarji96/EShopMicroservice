332f07ad430fbce1776e417f57cbf3665047e854
Hassan Marji <h.marji@reply.it>
2026-02-24 12:19:08 +0100

    - implementata feature get product by id - implemntato endpoint ed handler - implementata eccezione per product not found

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace CatalogAPI.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Handling GetProductByIdRequest called with query{query.Id}");
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            if (product is null)
+            {
+                logger.LogInformation($"Product with id {query.Id} not found");
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
