150b4470cc5943b382be8e2ed3cfcfaf6ac9899b
Hassan Marji <h.marji@reply.it>
2026-03-02 16:55:46 +0100

    - registrazione dei servizi carter, mediatR tramite DI nel program.cs

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -7,7 +7,7 @@ namespace Basket.API.Basket.StoreBasket
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/basket", async (StoreBasketRequest request, ISender sender) =>
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
             {
                 var command = request.Adapt<StoreBasketCommand>();
                 var result = await sender.Send(command);
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    cfg.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
