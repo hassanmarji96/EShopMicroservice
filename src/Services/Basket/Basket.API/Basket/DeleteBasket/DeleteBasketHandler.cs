2348ec0eb6f2331ee7b66d26030f11bb898107c0
Hassan Marji <h.marji@reply.it>
2026-03-02 14:28:09 +0100

    - sviluppate le feature per delete, store e get per il cestino, insieme a creazione dei modelli

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.API.Basket.DeleteBasket
+{
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasketVaidator : AbstractValidator<DeleteBasketCommand>
+    {
+        public DeleteBasketVaidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    internal class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            // TODO: delete basket from db and cache
+            // session.Delete<Product>(command.Id);
+            return new DeleteBasketResult(true);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart ShoppingCart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+    public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketValidator()
+        {
+            RuleFor(x => x.ShoppingCart).NotNull().WithMessage("Shopping cart cannot be null.");
+            RuleFor(x => x.ShoppingCart.UserName).NotEmpty().WithMessage("User name is required.");
+        }
+    }
+    internal class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart shoppingCart = command.ShoppingCart;
+            return new StoreBasketResult("swn");
+        }
+    }
+}
