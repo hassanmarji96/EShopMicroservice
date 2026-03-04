da0056489f454139b9abb620e60f0d188ee0b2ca
Hassan Marji <h.marji@reply.it>
2026-03-04 16:23:48 +0100

    - creato metodo per la retrieve dei discount sulla base del nome del prodotto

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,13 +1,25 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            //TODO: get discount from database
-            return base.GetDiscount(request, context);
+            
+            var coupon = await dbContext.Coupons
+                .FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+            if(coupon is null)
+                coupon = new Coupon { ProductName = "No discount", Amount = 0, Description = "No discount available" };
+
+            logger.LogInformation("[DiscountService] == [GetDiscount] --> Discount retrieved for ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
 
         public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
