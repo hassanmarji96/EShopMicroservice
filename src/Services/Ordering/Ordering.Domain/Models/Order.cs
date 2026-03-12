593edbf67c43e1ecf98975bf2a19a61c6f64ee26
Hassan Marji <h.marji@reply.it>
2026-03-12 15:03:40 +0100

    - rimosso problema di primitive obsession, inserendo degli ID fortemente tipizzati

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -6,12 +6,12 @@
     /// OrderPlacedEvent --> il nostro evento di dominio, che rappresenta un evento che si verifica quando un ordine viene effettuato. Questo evento può essere utilizzato 
     /// per notificare altri componenti del sistema o per eseguire azioni specifiche in risposta all'evento.
     /// </summary>
-    public class Order : Aggregate<Guid>
+    public class Order : Aggregate<OrderId>
     {
         private readonly List<OrderItem> _orderItems = new();
         private IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
-        public Guid CustomerId { get; private set; } = default!;
-        public string OrderName { get; private set; } = default!;
+        public CustomerId CustomerId { get; private set; } = default!;
+        public OrderName OrderName { get; private set; } = default!;
         public Address ShippingAddress { get; private set; } = default!;
         public Address BillingAddress { get; private set; } = default!;
         public Payment Payment { get; private set; } = default!;
