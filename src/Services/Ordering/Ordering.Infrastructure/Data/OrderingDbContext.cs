560a253ae88926c2a838f11d30d3430899b2ea6f
Hassan Marji <h.marji@reply.it>
2026-03-18 16:52:42 +0100

    - creata configurazione per le varie entità in code first - installate nuget packages per gestire ORM e CF migrations

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/OrderingDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Infrastructure.Data
+{
+    public class OrderingDbContext : DbContext
+    {
+        public OrderingDbContext(DbContextOptions<OrderingDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
