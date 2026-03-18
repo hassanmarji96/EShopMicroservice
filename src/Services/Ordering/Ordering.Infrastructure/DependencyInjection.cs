83645c8cfbd231c61824e98e82b4b78d04e09f08
Hassan Marji <h.marji@reply.it>
2026-03-18 17:57:04 +0100

    - creazione migrazione - inserimento connectionstring in appsettings.json - creato db in docker-compose per ordering

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -5,9 +5,15 @@ namespace Ordering.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, 
+            IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddDbContext<OrderingDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            //services.AddScoped<IOrderingDbContext, OrderingDbContext>();
             return services;
         }
     }
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using System.Reflection;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Domain.Enums;
+global using Ordering.Infrastructure.Data;
