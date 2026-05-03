b930b0ec9abe5d756402fbf29e7ea5a6bb19f699
Hassan Marji <h.marji@reply.it>
2026-05-03 14:32:45 +0200

    - configurazione message-broker nel servizio di mediazione

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace BuildingBlocks.Messaging.MassTransit
 {
     public static class Extensions
     {
-        public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
         {
+            services.AddMassTransit(cfg =>
+            {
+                cfg.SetKebabCaseEndpointNameFormatter();
+                if (assembly is not null)
+                   cfg.AddConsumers(assembly);
+
+                cfg.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host=>
+                    {
+                        host.Username(configuration["MessageBroker:Username"]!);
+                        host.Password(configuration["MessageBroker:Password"]!);
+                    });
+                    configurator.ConfigureEndpoints(context);
+                });
+            });
             return services;
         }
     }
